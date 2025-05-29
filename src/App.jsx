3d522973762100751c5d8ea703f1fc0be44e3dcb
Noah Christensen <noah.d.christensen@gmail.com>
2025-05-28 20:07:17 -0500

    bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // File: src/App.jsx
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import TeamPage from './pages/TeamPage';
 import ScrollToTop from './components/ScrollToTop';
